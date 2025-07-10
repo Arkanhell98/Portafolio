f008a425577218459a31755ee189bee1cc97afff
Arkanhell98 <gabopc98@gmail.com>
2025-07-10 12:20:12 -0500

    nuevo script

---
+++ scripts/contactScript.js
@@ -0,0 +1,9 @@
+ let swiper = new Swiper("#swiperContainer", {
+      loop:true,
+      spaceBetween: 0,
+      mousewheel: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
